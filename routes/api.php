c27b26236f94e404ba88fde3735cecbe25e0f059
Dariia Sliusar <dariiasliusar@gmail.com>
2025-03-28 14:10:27 +0100

    designing the Url

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,21 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//http://localhost:8800/api
+//http://localhost:8800/api/
 //universal resource locator - url
+//tickets
+//users
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function () {
+    return Ticket::all();
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
