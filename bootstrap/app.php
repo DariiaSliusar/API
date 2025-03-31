39f262269b1fb92b8eedc26dd745a5b6dadce665
Dariia Sliusar <dariiasliusar@gmail.com>
2025-03-31 08:13:12 +0200

    How to Version your API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,33 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        using: function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->group(base_path('routes/api_v1.php'));
+
+            Route::middleware('web')
+                ->prefix('web')
+                ->group(base_path('routes/web.php'));
+        },
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
     )
+//    ->withRouting(
+//        web: __DIR__.'/../routes/web.php',
+//        api: __DIR__.'/../routes/api.php',
+//        commands: __DIR__.'/../routes/console.php',
+//        health: '/up',
+//        then: function () {
+//            Route::middleware('api')
+//                ->prefix('api/v1')
+//                ->group(base_path('routes/api_v1.php'));
+//        },
+//
+//    )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,17 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//http://localhost:8800/api/
-//universal resource locator - url
-//tickets
-//users
+// http://localhost:8800/api/
+// universal resource locator - url
+// tickets
+// users
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('tickets', TicketController::class);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
