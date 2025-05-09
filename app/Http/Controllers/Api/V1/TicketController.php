9250d3836ff058ed55859cc85b17222b4c43b53d
Dariia Sliusar <dariiasliusar@gmail.com>
2025-05-09 10:41:12 +0200

    Documenting Your API with Scribe

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -16,8 +16,14 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 class TicketController extends ApiController
 {
     protected $policyClass = TicketPolicy::class;
+
     /**
-     * Display a listing of the resource.
+     * Get all tickets
+     *
+     * @group Managing Tickets
+     * @queryParam sort string Data field(s) to sort by. Separate multiple fields with commas. Denote descending order with a minus sign (-). Example: sort=title, -createdAt
+     * @queryParam filter[status] Filter by status code: A, C, H, X. No-example
+     * @queryParam filter[title] Filter by title. Wildcards are supported. Example: *fix*
      */
     public function index(TicketFilter $filters)
     {
@@ -25,7 +31,11 @@ class TicketController extends ApiController
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Create a ticket
+     *
+     * Creates a new ticket. Users can only create tickets for themselves. Managers can create tickets for any users.
+     *
+     * @group Managing Tickets
      */
     public function store(StoreTicketRequest $request)
     {
